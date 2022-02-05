c9e2f1078211ba70809f5f06891e63204e429875
Tom Stoepker <tom.stoepker@gmail.com>
2022-02-05 20:34:38 +0100

    task: setup project and basic slack integration

---
+++ config/settings.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type SlackSettings struct {
+	AuthToken string
+}
+
+type ServerSettings struct {
+	Host         string
+	Port         string
+	ProfilerPort string
+	DocsURL      string
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
+
+func Load() (ServerSettings, SlackSettings) {
+	err := godotenv.Load()
+	if err != nil {
+		panic(fmt.Sprintf("unable to load .env file: reason %v", err))
+	}
+
+	return ServerSettings{
+			Host:         os.Getenv("HOST"),
+			Port:         os.Getenv("PORT"),
+			DocsURL:      os.Getenv("DOCS_PATH"),
+			ProfilerPort: os.Getenv("PPROF"),
+			ReadTimeout:  getEnvAsDur("READ_TIMEOUT", time.Minute*2),
+			WriteTimeout: getEnvAsDur("WRITE_TIMEOUT", time.Minute*2),
+		}, SlackSettings{
+			AuthToken: os.Getenv("SLACK_AUTH_TOKEN"),
+		}
+}
+
+// getEnvAsDur returns a duration value for and environment variable key
+// if the key is empty or value is empty (or not in valid duration syntax)
+// this func returns the provided default
+func getEnvAsDur(key string, def time.Duration) time.Duration {
+	val := os.Getenv(key)
+	dur, err := time.ParseDuration(val)
+	if err != nil {
+		return def
+	}
+	return dur
+}
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/socketmode"
+)
+
+func main() {
+	// Load Env variables from .dot file
+	godotenv.Load(".env")
+
+	token := os.Getenv("SLACK_AUTH_TOKEN")
+	appToken := os.Getenv("SLACK_APP_TOKEN")
+	// Create a new client to slack by giving token
+	// Set debug to true while developing
+	// Also add a ApplicationToken option to the client
+	client := slack.New(token, slack.OptionDebug(true), slack.OptionAppLevelToken(appToken))
+	// go-slack comes with a SocketMode package that we need to use that accepts a Slack client and outputs a Socket mode client instead
+	socketClient := socketmode.New(
+		client,
+		socketmode.OptionDebug(true),
+		// Option to set a custom logger
+		socketmode.OptionLog(log.New(os.Stdout, "socketmode: ", log.Lshortfile|log.LstdFlags)),
+	)
+
+	socketClient.Run()
+}

d9bdc3e22574c56feb21a1f63d9070e6b8e49352
Tom Stoepker <tom.stoepker@biller.ai>
2022-03-09 16:23:17 +0100

    task: setup ping command

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"flag"
+	"net/http"
+
 	"github.com/syllabix/logger"
 	"github.com/syllabix/oncall/common/banner"
 	"github.com/syllabix/oncall/oncall"
@@ -8,6 +11,19 @@ import (
 )
 
 func main() {
+	ping := flag.Bool("ping", false, "pings the healthz endpoint")
+	flag.Parse()
+
+	if *ping {
+		log := logger.New()
+		res, err := http.Get("https://biller-oncall.herokuapp.com/healthz")
+		if err != nil {
+			log.Error("failed to ping", zap.Error(err))
+		}
+		log.Info("ping completed", zap.Int("status-code", res.StatusCode))
+		return
+	}
+
 	banner.Print()
 
 	app := oncall.NewApplication()
